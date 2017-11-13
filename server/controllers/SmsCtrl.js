1c6e15574c5e8b696cdb1cc23f982aec62e13671
Infoobjects <infoobjects@Akshay-MacBook.local>
2017-11-14 01:23:05 +0530

    SMS  -> Sms 2

--- server/controllers/SmsCtrl.js
+++ server/controllers/SmsCtrl.js
@@ -1,6 +1,6 @@
 var config = require('../scripts/config.json');
 var logger = require('../scripts/logger.js');
-var httpsService = require('../lib/HTTPSService.js');
+var smsSender = require('../lib/SmsSender.js');
 var systemVariable = require(config.outputFolder+'/variable.json');
 
 module.exports = function(app) {
@@ -8,10 +8,8 @@ module.exports = function(app) {
     controller.sendMessage = function(req, res, next) {
         var sendTo = req.body.mobileNumbers;
         var msg = req.body.msg;
-        var url = "http://nimbusit.co.in/api/swsendSingle.asp?username="+systemVariable.smsGatewayUsername+"&password="+ systemVariable.smsGatewayPassword +"&sender=" + systemVariable.smsGateWaySenderId + "&sendto=" +sendTo+ "&message="+msg;  
-        console.log(url);
-        httpsService.makeHTTPRequest(url, {}, function(data, statusCode) {
-            res.send(data);
+        smsSender.sendMessage(sendTo, msg, req, res, function(data, statusCode, req, res) {
+            res.send("Sent");
         })
     }
     return controller;
---
+++ server/lib/SmsSender.js
@@ -0,0 +1,14 @@
+var config = require('../scripts/config.json');
+var logger = require('../scripts/logger.js');
+var httpsService = require('../lib/HTTPSService.js');
+var systemVariable = require(config.outputFolder+'/variable.json');
+module.exports = {
+    sendMessage : function(sendTo, msg, req, res, cb) {
+        var sendTo = req.body.mobileNumbers;
+        var msg = req.body.msg;
+        var url = "http://nimbusit.co.in/api/swsendSingle.asp?username="+systemVariable.smsGatewayUsername+"&password="+ systemVariable.smsGatewayPassword +"&sender=" + systemVariable.smsGateWaySenderId + "&sendto=" +sendTo+ "&message="+msg;  
+        httpsService.makeHTTPRequest(url, {}, function(data, statusCode) {
+            cb(data, statusCode, req, res);
+        })
+    }
+}   
