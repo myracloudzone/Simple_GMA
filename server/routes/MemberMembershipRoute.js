5275f17ce3e70c0ca30f56ccfa09eb88127a00c3
Infoobjects <infoobjects@Akshay-MacBook.local>
2017-11-18 18:49:39 +0530

    Membership Module 2

--- server/routes/MemberMembershipRoute.js
+++ server/routes/MemberMembershipRoute.js
@@ -2,5 +2,8 @@ module.exports = function (app) {
     var memberMembershipCtrl = app.controllers.MemberMembershipCtrl;
     app.get('/membership/getMemebershipToMemberById', memberMembershipCtrl.getMemebershipToMemberById);
     app.post('/membership/addPayment', memberMembershipCtrl.addPayment);
-    app.get('/membership/getTransactionHistory', memberMembershipCtrl.getMemberTransactionHistory);  
+    app.get('/membership/getTransactionHistory', memberMembershipCtrl.getMemberTransactionHistory);
+    app.get('/membership/checkForMembershipUpgrade', memberMembershipCtrl.isMembershipUpgradeAllowed);
+    app.post('/membership/updateMembershipOfMember', memberMembershipCtrl.updateMembershipOfMember);
+    app.post('/membership/refundAmount', memberMembershipCtrl.refundAmount);
 };
