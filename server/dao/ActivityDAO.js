2112ded0770d71a3497aa30cff627c48fe50308b
Infoobjects <infoobjects@Akshay-MacBook.local>
2017-11-30 22:30:47 +0530

    New Handler for the Activity SMS send// Repeat Type

--- server/dao/ActivityDAO.js
+++ server/dao/ActivityDAO.js
@@ -15,6 +15,8 @@ module.exports = {
     },
     findAll : function(condition, req, res, callback) {
         console.log("--------------------------------DAO---------------------------------------------");
+        console.log("-------condition is ")
+        console.log(condition)
         schema.model('Activity').forge().where(condition).orderBy('name', 'ASC').fetchAll().then(function (result) {
             console.log("--------------------------------DAO1---------------------------------------------");
             if(result == null) {
