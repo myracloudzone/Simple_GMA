ba85424443233827261e64e23a062150a80e7fb5
Infoobjects <infoobjects@Akshay-MacBook.local>
2018-03-23 14:20:23 +0530

    New

--- server/routes/SMSRoute.js
+++ server/routes/SMSRoute.js
@@ -1,5 +1,6 @@
 module.exports = function (app) {
     var smsCtrl = app.controllers.SmsCtrl;
     app.post('/sms/sendMessage', smsCtrl.sendMessage);
-   
+    app.post('/sms/sendGroupMessage', smsCtrl.sendGroupMessage);
+    app.get('/sms/getSentMesssageHistory', smsCtrl.getSentMesssageHistory);
 };  
--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -2,4 +2,9 @@ module.exports = function (app) {
     var userCtrl = app.controllers.UserCtrl;
     app.get('/user/getLoggedInUser', userCtrl.getLoggedInUser);
     app.post('/user/changePassword', userCtrl.changePassword);
+    app.get('/user/list', userCtrl.list);
+    app.post('/user/createUser', userCtrl.createUser);
+    app.post('/user/updateUser', userCtrl.updateUser);
+    app.get('/user/getUser', userCtrl.getUser);
+    app.post('/user/deleteUser', userCtrl.deleteUser);
 };
