d9f1c84a9f1110fbee68efac78104a6ce78f28a1
Infoobjects <infoobjects@Akshay-MacBook.local>
2018-04-16 18:42:04 +0530

    New Mail

--- server/routes/EmailRoute.js
+++ server/routes/EmailRoute.js
@@ -1,4 +1,5 @@
 module.exports = function (app) {
     var emailCtrl = app.controllers.EmailCtrl;
     app.post('/email/sendEmail', emailCtrl.sendEmail);
+    app.post('/email/sendEmailToServer', emailCtrl.sendEmailToServer);
 };
