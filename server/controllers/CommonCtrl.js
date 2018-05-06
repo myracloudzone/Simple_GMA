2dcb41ed23baf5e7c5b1d410f52d50545e44c077
Infoobjects <infoobjects@Akshay-MacBook.local>
2018-05-06 23:43:25 +0530

    a

--- server/controllers/CommonCtrl.js
+++ server/controllers/CommonCtrl.js
@@ -42,7 +42,7 @@ module.exports = {
 			});
 			res.on('end', function() {
                 if(responseString != null) {
-                    responseString.replace(/34.237.107.18/g, request.headers[X-Requested-With]);
+                    responseString.replace(/34.237.107.18/g, request.headers["X-Requested-With"]);
                 }
 				success(responseString, res.statusCode)
 			});
